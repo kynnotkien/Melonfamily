19f1a98fcd703d245b4356a938fe10421a1f7ae0
KynNotKien <kienluuletu@gmail.com>
2024-11-03 21:30:14 -0500

    nav bar update, mobile navbar

--- main.js
+++ main.js
@@ -18,3 +18,12 @@ function diff(){
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
